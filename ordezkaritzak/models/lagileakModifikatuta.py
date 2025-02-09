af272459f021b63f353b09ff872f4ca2e0113244
Your Name <you@example.com>
2025-02-09 10:49:01 +0000

    añadir campo ordezkaritza a la facktura, falata; en el form de facturas añadir opcion obligatoria de elegir la ordezkaritza, y comprobar que el total que ha ganado el zuzendari sea el correcto,
    luego faltan varias validaciens en diferentes modulos

--- ordezkaritzak/models/lagileakModifikatuta.py
+++ ordezkaritzak/models/lagileakModifikatuta.py
@@ -8,3 +8,8 @@ class Langilea(models.Model):
     _inherit = 'hr.employee'
 
     ordezkaritza = fields.Many2one('ordezkaritzak.ordezkaritza', string="Ordezkaritza")
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    ordezkaritza = fields.Many2one('ordezkaritzak.ordezkaritza', string="Ordezkaritza")
--- ordezkaritzak/models/models.py
+++ ordezkaritzak/models/models.py
@@ -4,9 +4,9 @@ from odoo import models, fields, api
 from datetime import datetime
 
 ordezkaritza_motak = [
-        ('mota1', 'Lehen Mailaka'), 
-        ('mota2', 'Bigarren Mailakoa')
-    ]
+    ('mota1', 'Lehen Mailaka'),
+    ('mota2', 'Bigarren Mailakoa')
+]
 
 class Ordezkaritza(models.Model):
     _name = 'ordezkaritzak.ordezkaritza'
@@ -19,6 +19,20 @@ class Ordezkaritza(models.Model):
     nif = fields.Char(string="NIF", required=True)
     mota = fields.Selection(ordezkaritza_motak, string="Mota", required=True)
     probintzia = fields.Char(string="Probintzia", required=True)
+
+    @api.model
+    def create(self, vals):
+        # Crear el registro de Ordezkaritza
+        record = super(Ordezkaritza, self).create(vals)
+
+        # Crear una entrada en OrdezkaritzaDatuak para el zuzendaria actual
+        self.env['ordezkaritzak.datuak'].create({
+            'ordezkaritza': record.id,
+            'zuzendaria': record.zuzendaria.id,
+            'hasieraData': datetime.now(),
+        })
+
+        return record
 
     def write(self, vals):
         if 'zuzendaria' in vals:
@@ -52,28 +66,42 @@ class Ordezkaritza(models.Model):
                         'hasieraData': datetime.now(),
                     })
 
-
         return super(Ordezkaritza, self).write(vals)
 
     def unlink(self):
         for record in self:
             # Crear una entrada en OrdezkaritzaHistorikoa antes de eliminar el registro
-            historikoa =  self.env['ordezkaritzak.historikoa'].create({
-                'ordezkaritza': record.id,
-                'hasieraData': record.hasieraData,
-                'amieraData': datetime.now(),
-                'nif': record.nif,
-                'mota': record.mota,
-                'probintzia': record.probintzia,
-                'hasieraData': record.hasieraData
-            })
 
             datuak = self.env['ordezkaritzak.datuak'].search([
                         ('ordezkaritza', '=', record.id),
                         ('zuzendaria', '=', record.zuzendaria.id)
                     ], limit=1)
             
-            datuak.write({'historikoa': historikoa.id})
+            historikoa =  self.env['ordezkaritzak.historikoa'].create({
+                'izena': record.izena,
+                'hasieraData': datuak.hasieraData,
+                'amieraData': datetime.now(),
+                'nif': record.nif,
+                'mota': record.mota,
+                'probintzia': record.probintzia,
+            })
+
+            facturas = self.env['account.move'].search([
+                            ('ordezkaritza', '=', record.id),
+                            ('invoice_date', '>=', datuak.hasieraData),
+                            ('invoice_date', '<=', datetime.now())
+                        ])
+
+            total_irabazitako = 0.0
+            for factura in facturas:
+                total_irabazitako += factura.amount_total
+
+            
+            datuak.write({
+                'ordezkaritza': False,
+                'historikoa': historikoa.id,
+                'amieraData': datetime.now(),
+                'irabaziak': total_irabazitako})
 
         return super(Ordezkaritza, self).unlink()
 
@@ -81,8 +109,9 @@ class Ordezkaritza(models.Model):
 class OrdezkaritzaHistorikoa(models.Model):
     _name = 'ordezkaritzak.historikoa'
     _description = 'Ordezkaritza Historikoa'
+    _rec_name = 'izena'
 
-    ordezkaritza = fields.Many2one('ordezkaritzak.ordezkaritza', string="Ordezkaritza", required=True)
+    izena = fields.Char(string="Izena", required=True)
     hasieraData = fields.Date(string="Hasiera Data")
     amieraData = fields.Date(string="Amiera Data")
     nif = fields.Char(string="NIF", required=True)
@@ -93,7 +122,7 @@ class OrdezkaritzaDatuak(models.Model):
     _name = 'ordezkaritzak.datuak'
     _description = 'Ordezkaritza Datuak'
 
-    ordezkaritza = fields.Many2one('ordezkaritzak.ordezkaritza', string="Ordezkaritza", required=True)
+    ordezkaritza = fields.Many2one('ordezkaritzak.ordezkaritza', string="Ordezkaritza")
     zuzendaria = fields.Many2one('hr.employee', string="Zuzendaria", required=True)
     hasieraData = fields.Date(string="Hasiera Data")
     amieraData = fields.Date(string="Amiera Data")
